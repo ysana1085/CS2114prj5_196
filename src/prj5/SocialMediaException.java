8dbd4afdfcffaba0f9a7f2611b7dcf4e06d4d069
Bradley <bradley_roos@icloud.com>
2024-11-21 23:15:38 -0500

    add exception file

---
+++ src/prj5/SocialMediaException.java
@@ -0,0 +1,13 @@
+package prj5;
+public class SocialMediaException extends Exception
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+    public SocialMediaException(String s)
+    {
+        super(s);
+    }
+}
