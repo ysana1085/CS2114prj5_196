256c6e83aa3a53836b9c9ad1fa3e3e27a3146c67
ysana1085 <ysana1085@vt.edu>
2024-12-06 19:33:42 -0500

    Update CompareByMonthTest.java

--- src/prj5/CompareByMonthTest.java
+++ src/prj5/CompareByMonthTest.java
@@ -4,10 +4,10 @@ import student.TestCase;
 
 // -------------------------------------------------------------------------
 /**
- *  Test class of CompareByMonth
+ * Test class of CompareByMonth
  * 
- *  @author Yashvasin Sana
- *  @version Dec 6, 2024
+ * @author Yashvasin Sana
+ * @version Dec 6, 2024
  */
 public class CompareByMonthTest
     extends TestCase
@@ -17,14 +17,18 @@ public class CompareByMonthTest
     private CompareByMonth trad2;
     private CompareByMonth reach1;
     private CompareByMonth reach2;
-    
-    private Influencer left;
-    private Influencer right;
-    
+
     private InteractionData january;
     private InteractionData february;
     private InteractionData march;
     
+    private InteractionData january2;
+    private InteractionData february2;
+    private InteractionData march2;
+
+    private Influencer left;
+    private Influencer right;
+
     // ~ Constructors ..........................................................
 
     // ~Public Methods ........................................................
@@ -35,29 +39,28 @@ public class CompareByMonthTest
 
         reach1 = new CompareByMonth(false, 0);
         reach2 = new CompareByMonth(false, 1);
+
+        left = new Influencer("s", "s", "s", "s");
+        right = new Influencer("s", "s", "s", "s");
+
+        january = new InteractionData(10, 10, 10, 10, 2, "January");
+        february = new InteractionData(20, 20, 20, 20, 10, "February");
+        march = new InteractionData(30, 30, 30, 30, 10, "March");
         
-        Influencer left = new Influencer("s","s","s","s");
-        Influencer right = new Influencer("s","s","s","s");
-        
-        InteractionData january = 
-                    new InteractionData(10, 10, 10, 10, 2, "January");
-        
-        InteractionData february = 
-                    new InteractionData(20, 20, 20, 20, 10, "February");
-        
-        InteractionData march = 
-                    new InteractionData(30, 30, 30, 30, 10, "March");
+        january2 = new InteractionData(10, 10, 10, 10, 1, "January");
+        february2 = new InteractionData(20, 20, 20, 20, 5, "February");
+        march2 = new InteractionData(30, 30, 30, 30, 5, "March");
     }
 
 
     public void testCompare()
     {
-        left.getMonthData().add(january);
         left.getMonthData().add(february);
         left.getMonthData().add(march);
-        
-        right.getMonthData().add(january);
-        
-        assertEquals(trad1.compare(left, right), 0);
+        left.getMonthData().add(january);
+
+        right.getMonthData().add(january2);
+
+        assertEquals(0, trad1.compare(left, right));
     }
 }
